c233861f2ff52d9419f1f47afca92c92e32e2ef9
luojiyang <luojiyang115@163.com>
2024-10-20 16:11:46 +0800

    video页面绘制，对接接口

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,66 +1,62 @@
-// pages/video/video.js
+const app = getApp();
+const url = app.globalData.url;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    page: 1,   //请求页面参数
+    size: 5,   //每次请求视频个数
+    videos: [],   //视频列表
+    posterInfo: {   //视频发布者信息
+      avatar: '',
+      posterName: ''
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad(options) {
-
+    this.setData({
+      page: 1
+    })
+    this.getVideo(this.data.page, this.data.size)
+    this.getPosterInfo()
+  },
+  getVideo: function (page, size) {   //获取视频，传入请求页和每页视频个数
+    let that = this
+    wx.request({
+      url: url + '/video',
+      method: 'GET',
+      data: {
+        page: page,
+        size: size
+      },
+      success(res) {
+        if (res.data.code == 20002) {
+          let videos = [...that.data.videos, ...res.data.data];
+          that.setData({
+            page: page + 1,
+            videos: videos
+          })
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  onReachBottom() {     // 触底更新，刷新数据
+    this.getVideo(this.data.page, this.data.size)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  getPosterInfo: function () {   //获取发布者信息
+    let that = this
+    wx.request({
+      url: url + '/user',
+      method: 'GET',
+      success(res) {
+        if (res.data.code == 20000) {
+          let posterInfo = {
+            avatar: res.data.data.avatar,
+            posterName: res.data.data.username
+          }
+          that.setData({
+            posterInfo: posterInfo
+          })
+        }
+      }
+    })
   }
 })
